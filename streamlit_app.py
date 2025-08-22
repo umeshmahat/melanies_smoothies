fb9f962ae1d1e13a9b0695d6d21458ee52dd638d
umeshmahat <umesh.mahat@cbre.com>
2025-08-22 14:05:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,12 +54,16 @@ if Ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-#new section to display smoothiefroot nutrition information
+
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response)
-st.text(smoothiefroot_response.json())
-#sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width= True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width= True)
 
+if ingredients_list:
+  ingredients_string=''
 
+  for fruit_chosen in ingredients_list:
+    ingredients_string += fruit_chose+' '
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width= True)
         
